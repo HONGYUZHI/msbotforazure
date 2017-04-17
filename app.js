15f936d68640b3575ba1d48a364cd69e93d2cf9d
uzi <a25395599@gmail.com>
2017-04-17 16:34:09 +0800

    555

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "123",
+    appPassword: "321"
 });
 console.log('%s | %s', process.env.MICROSOFT_APP_ID, process.env.MICROSOFT_APP_PASSWORD); 
 var bot = new builder.UniversalBot(connector);
